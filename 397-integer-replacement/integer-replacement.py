0131f3ab4274eac8ba3775f1e959bb9b061f8aa8
Varun Vishwakarma <vishwakarmavarun13@gmail.com>
2026-09-07 20:28:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (69.70%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.py
@@ -0,0 +1,19 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        count = 0
+        while n > 1:
+            if n%2 == 0:
+                n = n/2
+                count += 1
+            else :
+                if ((n+1)/(n-1))%2 == 0:
+                    n = n-1 
+                    count += 1
+                else :
+                    if ((n-1)/2)%2 == 0 :
+                        n = n-1
+                        count += 1
+                    else :
+                        n = n + 1
+                        count += 1
+        return count
