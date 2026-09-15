32b83841393d57998d8ac9aa4133a3e1be4d2256
Varun Vishwakarma <vishwakarmavarun13@gmail.com>
2026-09-15 14:43:27 +0530

    Time: 8 ms (29.53%) | Memory: 19.3 MB (31.01%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.py
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -1,7 +1,7 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         i = 0
-        score = -1
+        score = 0
         while i < len(nums):
             score = max(nums[:i+1]) - min(nums[i:])
             if score <= k:
